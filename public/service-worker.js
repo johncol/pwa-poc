49590e5123bba8c07d733af778afb8b9fc443b06
John <john.19col@gmail.com>
2018-01-08 20:14:31 -0500

    Chapter 5: caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,56 @@
 
+const CacheType = {
+  STATIC_CONTENT: 'v1-static-content',
+  DYNAMIC_CONTENT: 'v2-dynamic-content',
+};
+
+const nonCacheableRequestProtocols = ['chrome-extension'];
+
+let cacheStaticResources = function (cache) {
+  console.log('Caching static content..');
+  cache.addAll([
+    '/',
+    '/index.html',
+    '/favicon.ico',
+    '/src/js/app.js',
+    '/src/js/feed.js',
+    '/src/js/material.min.js',
+    '/src/css/app.css',
+    '/src/css/feed.css',
+    '/src/images/main-image.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+  ]);
+};
+
 self.addEventListener('install', event => {
   console.log('Service worker installed:', event);
+  event.waitUntil(caches.open(CacheType.STATIC_CONTENT).then(cacheStaticResources));
 });
 
 self.addEventListener('activate', event => {
   console.log('Service worker activated:', event);
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys
+    .filter(key => key !== CacheType.STATIC_CONTENT && key !== CacheType.DYNAMIC_CONTENT)
+    .map(key => caches.delete(key))
+  )));
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch event triggered:', event.request.method + ' ' + event.request.url);
+  event.respondWith(caches.match(event.request)
+    .then(cachedResponse => {
+        console.log('Fetch:', event.request.method + ' ' + event.request.url);
+        console.log('  cached response is:', cachedResponse);
+        return cachedResponse ? cachedResponse : fetch(event.request).then(response => {
+          return caches.open(CacheType.DYNAMIC_CONTENT).then(cache => {
+            let url = event.request.url;
+            if (!nonCacheableRequestProtocols.find(protocol => url.startsWith(protocol))) {
+              cache.put(url, response.clone());
+            }
+            return response;
+          });
+        });
+      }));
 });
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,8 +1,9 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+let shareImageButton = document.querySelector('#share-image-button');
+let createPostArea = document.querySelector('#create-post');
+let closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+let sharedMomentsArea = document.querySelector('#shared-moments');
 
-function openPwaPrompt(installPwa) {
+let openPwaPrompt = function (installPwa) {
   installPwa.prompt();
   installPwa.userChoice.then(result => {
     console.log('PWA prompt result:', result);
@@ -12,20 +13,55 @@ function openPwaPrompt(installPwa) {
       console.log('PWA installation accepted..');
     }
   });
-}
+};
 
-function openCreatePostModal() {
+let openCreatePostModal = function () {
   createPostArea.style.display = 'block';
   if (deferredPwaPrompt) {
     openPwaPrompt(deferredPwaPrompt);
     deferredPwaPrompt = null;
   }
-}
+};
 
-function closeCreatePostModal() {
+let closeCreatePostModal = function () {
   createPostArea.style.display = 'none';
-}
+};
 
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+function onSaveButtonClicked(event) {
+  console.log('clicked');
+}
+
+function createCard() {
+  var cardWrapper = document.createElement('div');
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+  var cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
+  cardTitle.style.backgroundSize = 'cover';
+  cardTitle.style.height = '180px';
+  cardWrapper.appendChild(cardTitle);
+  var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.style.color = 'white';
+  cardTitleTextElement.className = 'mdl-card__title-text';
+  cardTitleTextElement.textContent = 'San Francisco Trip';
+  cardTitle.appendChild(cardTitleTextElement);
+  var cardSupportingText = document.createElement('div');
+  cardSupportingText.className = 'mdl-card__supporting-text';
+  cardSupportingText.textContent = 'In San Francisco';
+  cardSupportingText.style.textAlign = 'center';
+  var cardSaveButton = document.createElement('button');
+  cardSaveButton.textContent = 'Save';
+  cardSaveButton.addEventListener('click', onSaveButtonClicked);
+  cardSupportingText.appendChild(cardSaveButton);
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+fetch('https://httpbin.org/get')
+  .then(res => res.json())
+  .then(data => createCard());
